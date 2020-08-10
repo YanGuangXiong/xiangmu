ca64a0257a2fe4af7e4ae5733dae3d2a07f72cff
小闫 <1561265951@qq.com>
2020-08-10 21:00:51 +0800

    1

--- src/network/purchases.js
+++ src/network/purchases.js
@@ -13,22 +13,3 @@ export function GetpurcharesList(pagindex, limit) {
     })
 }
 
-// 添加
-export function Addpurchares(obj) {
-    return ajaxRequest({
-        method: 'post',
-        url: `api/purchase/Savet_billList`,
-        data: JSON.stringify(obj)
-    })
-}
-
-// 删除
-export function Deletepurchares(obj) {
-    return ajaxRequest({
-        method: 'Delete',
-        url: `api/purchase/DeleteInfo`,
-        params:{
-            billId:obj
-        }
-    })
-}
